508f2f16788782dc39564b74755d84b83ebcad37
mojidev-py <pycharmdudeig@gmail.com>
2024-09-02 10:13:11 -0700

    equal strings

---
+++ equalstrings.py
@@ -0,0 +1,7 @@
+str1 = input("Enter the first string.")
+str2 = input("Enter the second string.")
+
+if str1 == str2:
+    print("These strings are equal.")
+else:
+    print("These strings aren't equal.")
